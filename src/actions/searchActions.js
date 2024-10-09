1eb3990474eaf2434482484ab3bc745746ace71b
geggenpress4231 <ami.utsarabbani@gmail.com>
2024-10-09 14:43:00 -0400

    code cleanup

--- src/actions/searchActions.js
+++
@@ -1,7 +0,0 @@
-import { UPDATE_SEARCH_PARAMS } from "./actionTypes";
-
-// Action to update search parameters in the store
-export const updateSearchParams = (params) => ({
-  type: UPDATE_SEARCH_PARAMS,
-  payload: params,
-});
